8e81685426974d2822611aaa34e8c5a87e55ae4c
Amir <ataiari@stud.hs-offenburg.de>
2022-01-22 17:06:58 +0100

    blatt10

--- js/infothek.js
+++ js/infothek.js
@@ -1,34 +1,25 @@
 "use stricts";
 function initialize() {
-let map = L.map('map').setView([48.225478  , 8.3852168], 9.5);
+    let map = L.map('map').setView([48.4716556, 7.944378], 9);
 
-/*Kehl: 48.5728929  7.8109768
-
-VS: 48.06034469604492 8.458127975463867
-Freiburg: 48.7580777 8.2415239
-*/
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
+    center: map,
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'your.mapbox.access.token'
-}).addTo(map);
+    }).addTo(map);
+    L.marker([47.9960901, 7.8494005]).bindPopup('Landesgartenschau 1986 in Freiburg im Breisgau').addTo(map),
+    L.marker([48.338896, 7.8710825]).bindPopup('Landesgartenschau 2018 in Lahr').addTo(map),
+    L.marker([48.56852819515478, 7.799598296341603]).bindPopup('Landesgartenschau 2004 in Kehl').addTo(map),
+    L.marker([48.57580707448527, 7.774736102911195]).bindPopup('Landesgartenschau 2004 in Straßburg').addTo(map);
+    L.marker([48.7580777, 8.2415239]).bindPopup('Landesgartenschau 1981 in Baden-Baden').addTo(map);
 
-
-
-var popup = L.popup()
-    .setLatLng([48.5728929, 8.2415239])
-    .setContent("Landesgartenschau 2004")
-    .openOn(map);
-
-var popup = L.popup()
-    .setLatLng([48.7580777, 7.8109768])
-    .setContent("Landesgartenschau 1986")
-    .openOn(map);
 }
+
+
 /*Standortzulassung*/
 /*
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
@@ -40,6 +31,4 @@ function successLocation(position){
 }
 function errorLocation(){}
 }
-
-
 */
