dd296f24e9dfce5aebf3a8b18b073464f1280872
Amir <ataiari@stud.hs-offenburg.de>
2022-01-27 20:17:36 +0100

    abgabe11

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
-function playVideo(){
+"use stricts";
 
-    $("#indexV").get(0).play();
-}
+var video = document.getElementById("vid");
+var playbtn = document.getElementById("playbtn");
+var pausebtn = document.getElementById("pausebtn");
 
-function pauseVideo(){
-    $("#indexV").get(0).pause();
+
+function Play() {
+    if(video.paused) {
+        video.play();
+        btn.style.background = "url(play.png) no-repeat";
+    }
+    else{
+        video.pause();
+        btn.style.background = "url(pause.png) no-repeat";
+    }
 }
